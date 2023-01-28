451be0ff778337b43717681d5eba3fe070df6d6a
camilabv14 <116381831+camilabv14@users.noreply.github.com>
2023-01-28 10:25:48 -0500

    Update mymoney.js

--- src/closure/mymoney.js
+++ src/closure/mymoney.js
@@ -1,7 +1,23 @@
-function moneyBox(coins) {
+/* function moneyBox(coins) {
   let saveCoins = 0;
   saveCoins += coins;
   console.log(`MoneyBox: $${saveCoins}`);
 }
 moneyBox(5);
 moneyBox(5);
+ */
+
+function moneyBox(coins) {
+  let saveCoins = 0;
+  function countCoins(coins) {
+    saveCoins += coins;
+    console.log(`MoneyBox: $${saveCoins}`);
+  }
+  return countCoins;
+}
+const myMoneyBox = moneyBox();
+
+myMoneyBox(5)
+myMoneyBox(15)
+
+const moneyBoxAna = moneyBox();
